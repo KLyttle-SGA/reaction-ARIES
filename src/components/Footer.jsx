e886a44637146286e55f2991115e37b36093327b
Kaden Lyttle <kaden.lyttle@sgasoftware.com>
2022-12-20 10:31:29 -0500

    Fix wacky appearance on Galaxy Fold.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
 		<div className="footer">
           <p>Stock, Garber, and Associates - {year}</p>
           <p className="small-footer">Kaden Lyttle, Liam Creek</p>
-          <p className="smaller-footer">(v. rA-Alpha)</p>
+          <p className="smaller-footer">v Alpha : This stuff does NOT work!</p>
         </div>
       </div>
   );
