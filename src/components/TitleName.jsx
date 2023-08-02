2420ea908d5eca436514c89f8cd2b5ae14a60fda
Kaden Lyttle <kaden.lyttle@sgasoftware.com>
2023-08-02 16:23:44 -0400

    Change animation, tag count error, title letters now in span for future animation testing.

--- src/components/TitleName.jsx
+++ src/components/TitleName.jsx
@@ -7,22 +7,22 @@ function TitleName() {
 
 			<div className="title-container">
 				<div className="title">
-				<h1 id="title-text" className="">A R I E S</h1>
-				<p className="subtitle">
-					{/*<span className="sub-bold">R</span>EACTING
+					<h1 id="title-text" className=""><span class="title-a">A</span> <span class="title-r">R</span> <span class="title-i">I</span> <span class="title-e">E</span> <span class="title-s">S</span></h1>
+					{/*<p className="subtitle">
+					<span className="sub-bold">R</span>EACTING⠀
 					<span className="sub-bold">A</span>GGREGATION⠀OF⠀
 					<span className="sub-bold">R</span>ESTAURANTS⠀FOR⠀
 					<br className="enable-title-break"/>
 					THE⠀
-					<span className="sub-bold">I</span>DLE,
+					<span className="sub-bold">I</span>DLE,⠀
 					<span className="sub-bold">T</span>ENACIOUS,⠀AND⠀
 					<span className="sub-bold">I</span>NSATIABLE⠀
 					<span className="sub-bold">E</span>MPLOYEES⠀OF⠀
-					<span className="sub-bold">S</span>GA*/}
-					</p>
+					<span className="sub-bold">S</span>GA
+					</p>*/}
 				</div>
 
-				<Tags maxTagCount="10"/>
+				<Tags maxTagCount="12"/>
 
 			</div>
 
